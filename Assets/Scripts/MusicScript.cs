8e1ec2df41df640ba7789962e5a3526c8cf7900e
Nima <Nima@ws-92-49.haw-1x.haw-hamburg.de>
2016-12-01 16:08:10 +0100

    Music Added

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicScript : MonoBehaviour {
+	AudioSource menumusic;
+	static bool AudioBegin = false;
+
+	void Start() {
+		menumusic.loop = true;
+	}
+
+	void Awake()
+	{
+		menumusic = GetComponent<AudioSource>();
+
+		if (AudioBegin && Application.loadedLevelName == "Menu") {
+			Destroy (gameObject);
+		}
+
+		if (!AudioBegin) {
+			menumusic.Play ();
+			DontDestroyOnLoad (gameObject);
+			AudioBegin = true;
+		} 
+	}
+	void Update () {
+		if(Application.loadedLevelName == "Green" || Application.loadedLevelName == "Purple" || Application.loadedLevelName == "Yellow")
+		{
+			menumusic.Stop();
+			AudioBegin = false;
+		}
+	}
+}
